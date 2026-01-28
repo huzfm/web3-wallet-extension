f80c86c189cf10aa50f70fcdc1557938d044f075
huzfm <huzaaifmushtaq@gmail.com>
2026-01-28 19:50:24 +0530

    feat: Add Mnemonic component and useCopy hook for displaying and copying mnemonic phrases.

--- src/components/mnemonic.tsx
+++ src/components/mnemonic.tsx
@@ -1,18 +1,18 @@
-import { useState } from "react"
+import { useCopy } from "../hooks/useCopy"
 
 type Props = {
       mnemonic: string
 }
 
 export function Mnemonic({ mnemonic }: Props) {
-      const [copied, setCopied] = useState("")
-
-      function handleCopy() {}
+      const { copied, copy } = useCopy()
       return (
             <>
                   <div>Mnemonic</div>
-                  {mnemonic}
-                  <p>sdkjfnsk</p>
+                  {mnemonic}{" "}
+                  <button onClick={() => copy(mnemonic)}>
+                        {copied ? "copied" : "copy"}
+                  </button>{" "}
             </>
       )
 }
---
+++ src/hooks/useCopy.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+export function useCopy() {
+      const [copied, setCopied] = useState(false)
+      const copy = async (text: string) => {
+            await navigator.clipboard.writeText(text)
+            setCopied(true)
+            setTimeout(() => setCopied(false), 1500)
+      }
+      return { copied, copy }
+}
